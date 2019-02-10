7406d69f90df6748d8e591c7e3547c1f37cd4ac5
Joe Nelson <cred+github@begriffs.com>
2019-02-09 23:55:13 -0600

    Mysterious failure on second connection

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAGS = -std=c99 -Wall -Wextra -Wpedantic -Wshadow `pkg-config --cflags libgit2`
+CFLAGS = -std=c99 -D_POSIX_C_SOURCE=200809L -Wall -Wextra -Wpedantic -Wshadow `pkg-config --cflags libgit2`
 
 LDFLAGS = `pkg-config --libs libgit2`
 
--- gitftp.c
+++ gitftp.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <netdb.h>
 
 #include <git2/errors.h>
 #include <git2/global.h>
 #include <git2/repository.h>
 #include <git2/revparse.h>
 #include <git2/tree.h>
+
+#define DEFAULT_PORT "8021"
+#define TCP_BACKLOG  0
 
 void git_or_die(int code)
 {
@@ -29,26 +36,94 @@ int pr_node(const char *root, const git_tree_entry *entry, void *payload)
 	return 0;
 }
 
+/* adapted from
+ * http://pubs.opengroup.org/onlinepubs/9699919799/functions/getaddrinfo.html
+ */
+int bind_or_die(char *svc)
+{
+	int sock, e;
+	struct addrinfo hints = {0}, *addrs, *ap;
+
+	hints.ai_family   = AF_UNSPEC;  /* IPv4 or IPv6 */
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags    = AI_PASSIVE; /* have system provide IP */
+	hints.ai_protocol = 0;
+	if ((e = getaddrinfo(NULL, svc, &hints, &addrs)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(e));
+        exit(EXIT_FAILURE);
+    }
+
+    for (ap = addrs; ap != NULL; ap = ap->ai_next)
+    {
+        sock = socket(ap->ai_family, ap->ai_socktype, ap->ai_protocol);
+        if (sock < 0)
+            continue;
+        if (bind(sock, ap->ai_addr, ap->ai_addrlen) == 0)
+            break; /* noice */
+        perror("Failed to bind");
+        close(sock);
+    }
+
+    if (ap == NULL)
+    {
+        fprintf(stderr, "Could not bind\n");
+        exit(EXIT_FAILURE);
+    }
+    freeaddrinfo(addrs);
+
+    return sock;
+}
+
+void serve(git_tree *tr)
+{
+    int sock = bind_or_die(DEFAULT_PORT), conn;
+    struct sockaddr client;
+    socklen_t clientsz = sizeof client;
+    (void)tr;
+
+    /* choosing a minimal backlog until experience
+     * proves that a longer one is advantageous */
+	if (listen(sock, TCP_BACKLOG) < 0)
+    {
+        perror(NULL);
+        exit(EXIT_FAILURE);
+    }
+
+    while (1)
+    {
+        if ((conn = accept(sock, &client, &clientsz)) < 0)
+        {
+            printf("! %i on %i\n", conn, sock);
+            /* perror("Failed accepting connection"); */
+            close(sock);
+            exit(EXIT_FAILURE);
+        }
+        printf("  %i on %i\n", conn, sock);
+        write(conn, "Bye\n", 4);
+        close(conn);
+    }
+}
+
+
 int main(int argc, char **argv)
 {
 	git_repository *repo;
 	git_object *obj;
-
-	git_or_die( git_libgit2_init() );
-	atexit(cleanup);
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: %s repo-path\n", *argv);
 		return EXIT_FAILURE;
 	}
-	
+
+	git_or_die( git_libgit2_init() );
+	atexit(cleanup);
+
 	git_or_die( git_repository_open(&repo, argv[1]) );
 	git_or_die( git_revparse_single(&obj, repo, "HEAD^{tree}") );
 
-	git_or_die(git_tree_walk(
-		(git_tree *)obj, GIT_TREEWALK_POST, pr_node, NULL
-	));
+	serve((git_tree *)repo);
 
 	return EXIT_SUCCESS;
 }
